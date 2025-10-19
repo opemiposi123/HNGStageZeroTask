4dc976a638d11b7a5c8c40b8e59d201df6a42982
AbdulWaheedMahmoodAhmad <abdulwaheedmahmoodahmad@gmail.com>
2025-10-19 09:25:32 +0100

    Add project files.

---
+++ HNGStageZeroTask/CatFactService.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+
+namespace HNGStageZeroTask
+{
+    public interface ICatFactService
+    {
+        Task<string> GetRandomCatFactAsync();
+    }
+
+    public class CatFactService : ICatFactService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CatFactService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<string> GetRandomCatFactAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            try
+            {
+                var response = await client.GetAsync("https://catfact.ninja/fact");
+                response.EnsureSuccessStatusCode();
+
+                var json = await response.Content.ReadAsStringAsync();
+                using var doc = JsonDocument.Parse(json);
+
+                return doc.RootElement.GetProperty("fact").GetString();
+            }
+            catch
+            {
+                // Return fallback message if API fails
+                return "Cats are wonderful animals, but our cat fact service is currently unavailable.";
+            }
+        }
+    }
+}
---
+++ HNGStageZeroTask/Controllers/CatFactController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HNGStageZeroTask.Controllers
+{
+    [Route("me")]
+    [ApiController]
+    public class CatFactController : ControllerBase
+    {
+        private readonly ICatFactService _catFactService;
+
+        public CatFactController(ICatFactService catFactService) 
+        {
+            _catFactService = catFactService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProfileAsync()
+        {
+            var catFact = await _catFactService.GetRandomCatFactAsync();
+
+            var result = new
+            {
+                status = "success",
+                user = new
+                {
+                    email = "andulwaheedmahmoodahmad6@gmail.com",
+                    name = "AbdulWaheed MahmoodAhmad",
+                    stack = "C#/.NET Core"
+                },
+                timestamp = DateTime.UtcNow.ToString("o"),
+                fact = catFact
+            };
+
+            return Ok(result);
+        }
+    }
+}
